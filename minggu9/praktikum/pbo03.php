d4577f333a708bc237195ac73d0f3184a0fd46c4
Faris-0 <faris.abdulh@gmail.com>
2021-05-30 16:55:00 +0700

    Praktikum dan Latihan

--- minggu9/praktikum/pbo03.php
+++ minggu9/praktikum/pbo03.php
@@ -1,8 +1,5 @@
 <?php
-<<<<<<< HEAD:minggu9/praktikum/pbo03.php
-=======
 //include "pbo02.inc.php";
->>>>>>> b7c6bcaff60534a9e710c4b9e11b591b360c92a9:minggu9/praktikum/pbo3.php
 include "pbo02.php";
 echo "<html><head><title>Mahasiswa</title></head><body>";
 $form = new Form ("","Input Form");
